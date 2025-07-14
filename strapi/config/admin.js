dd14bd3a47199b3a11cd486178e4f4119123d6e0
Mathis Thomsen <matthomster@gmail.com>
2025-07-14 12:14:50 +0200

    Fixes on variable mapping

--- strapi/config/admin.js
+++ strapi/config/admin.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
     secret: env('ADMIN_JWT_SECRET'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('ADMIN_API_TOKEN_SALT'),
   },
   transfer: {
     token: {
